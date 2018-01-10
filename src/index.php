dec36dca71899291fc791883a91af4efff0e32a7
lukaszurbanski <lukasz.urbanski@icloud.com>
2018-01-10 19:55:07 +0100

    Load balancer

--- src/index.php
+++ src/index.php
@@ -4,5 +4,6 @@
     $password = "default";
     $database = "default";
     $conn = mysqli_connect($host, $username, $password, $database) or die("Connection failed: " . mysqli_connect_error());
-    echo "Connected successfully";
+    echo "Connected successfully\n\n\n";
     mysqli_close($conn);
+    var_dump($_SERVER['HOSTNAME']);
